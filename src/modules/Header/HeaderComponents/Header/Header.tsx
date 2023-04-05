491779d85e1f955be88f04b0240502a770ec5ce4
Petrov Danil <cras.petrov@yandex.ru>
2023-04-05 06:59:51 +0200

    adaptive header and titles

--- src/modules/Header/HeaderComponents/Header/Header.tsx
+++ src/modules/Header/HeaderComponents/Header/Header.tsx
@@ -1,20 +1,22 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import { motion, useScroll, useSpring } from 'framer-motion'
 import { NavLink } from 'react-router-dom'
 
-import { HeaderLink, HeaderLinkType } from '../HeaderLink';
+import { HeaderLink, HeaderLinkType } from '../HeaderLink/HeaderLink';
 import Button from '../../../../UI/Button/Button';
 
-import classes from './Header.module.scss'
 import planImage from '../../../../assets/icons/plan.svg'
 import messageImage from '../../../../assets/icons/message.svg'
 import logo from '../../../../assets/icons/logo.png';
 
 import { email, tel, links } from '../../HeaderStore/headerConsts'
 
+import classes from './Header.module.scss'
+
 
 export const Header = memo(() => {
     const { scrollYProgress } = useScroll();
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
     const scaleX = useSpring(scrollYProgress, {
         stiffness: 150,
         damping: 30,
@@ -23,63 +25,81 @@ export const Header = memo(() => {
     const onLinkClick = () => {
         scaleX.set(0)
         window.scrollTo(0, 0);
+        setIsMenuOpen(false)
+    }
+    const onBurgerClick = () => {
+        setIsMenuOpen((prev) => !prev)
     }
     return (
         <header className={classes.header}>
             <div className='container'>
-                <nav className={classes.nav}>
-                    <div className={classes.linksList}>
-                        <img
-                            className={classes.navLogo}
-                            src={logo}
-                            alt='logo'>
-                        </img>
-                        {links.map(link => {
-                            return (
-                                <NavLink
-                                    to={link.link}
-                                    key={link.link}
-                                    className=
-                                    {
-                                        ({ isActive }) =>
-                                            [classes.link, isActive ? classes.linkActive : ''].join(' ')
-                                    }
-                                    onClick={onLinkClick}
-                                >
-                                    {link.title}
-                                </NavLink>
-                            )
-                        })}
+                <div className={classes.headerInner}>
+                    <img
+                        className={classes.logo}
+                        src={logo}
+                        alt='logo'>
+                    </img>
+                    <nav className={`${classes.nav} ${isMenuOpen && classes.navActive}`}>
+                        <div className={classes.linksList}>
+                            {links.map(link => {
+                                return (
+                                    <NavLink
+                                        to={link.link}
+                                        key={link.link}
+                                        className=
+                                        {
+                                            ({ isActive }) =>
+                                                [classes.link, isActive ? classes.linkActive : ''].join(' ')
+                                        }
+                                        onClick={onLinkClick}
+                                    >
+                                        {link.title}
+                                    </NavLink>
+                                )
+                            })}
+                        </div>
+                        <ul className={classes.contactsList}>
+                            <HeaderLink
+                                headerLinkType={HeaderLinkType.email}
+                                href={email}
+
+                            />
+                            <HeaderLink
+                                headerLinkType={HeaderLinkType.phone}
+                                href={tel}
+                            />
+                        </ul>
+                    </nav>
+                    <div  className={classes.buttons}>
+                        <Button
+                            className={classes.modalBtn}
+                            beforeImg={planImage}
+                            afterImg={messageImage}
+                        >
+                            Написать обращение
+                        </Button>
+                        <button
+                            className={
+                                `${classes.burgerBtn} ${isMenuOpen
+                                    ? classes.burgerClose
+                                    : ''
+                                }`}
+                            onClick={onBurgerClick}
+                        >
+                            <span />
+                            <span />
+                            <span />
+                        </button>
                     </div>
-                    <ul className={classes.contactsList}>
-                        <HeaderLink
-                            headerLinkType={HeaderLinkType.email}
-                            href={email}
 
-                        />
-                        <HeaderLink
-                            headerLinkType={HeaderLinkType.phone}
-                            href={tel}
-                        />
-                        <li>
-                            <Button
-                                className={classes.btn}
-                                beforeImg={planImage}
-                                afterImg={messageImage}
-                            >
-                                Написать обращение
-                            </Button>
-                        </li>
-                    </ul>
 
-                </nav>
+
+                </div>
             </div >
             <motion.div
                 className={classes.scrollProgress}
                 style={{ scaleX: scaleX }}
             />
-
-
         </header >
     )
 })
---
+++ src/modules/Header/HeaderComponents/HeaderLink/HeaderLink.tsx
@@ -0,0 +1,36 @@
+import { FC, ReactNode, memo } from 'react'
+import classes from './HeaderLink.module.scss'
+
+export enum HeaderLinkType {
+    phone =  classes.phone,
+    email =  classes.email,
+}
+interface IHeaderLink {
+    headerLinkType?: HeaderLinkType,
+    href: string,
+    children?: ReactNode
+}
+export const HeaderLink: FC<IHeaderLink> = memo((props) => {
+    const { headerLinkType, href, children } = props
+    let className: string = classes.contactLink 
+    let link: string = href
+    if (headerLinkType === HeaderLinkType.email) {
+        className = [className, HeaderLinkType.email].join(' ')
+        link = `mailto:${link}`
+    }
+    else if (headerLinkType === HeaderLinkType.phone) {
+        className  = [className, HeaderLinkType.phone].join(' ')
+        link = `tel:${link}`
+    }
+
+    return (
+        <li>
+            <a
+                className={className}
+                href={link}
+            >
+                {children ? children : href}
+            </a>
+        </li>
+    )
+})
