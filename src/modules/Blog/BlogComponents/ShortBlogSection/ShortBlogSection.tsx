8e3db13b008a42e5aceb472b431419bba61637e0
Petrov Danil <cras.petrov@yandex.ru>
2023-06-22 22:35:08 +0200

    Удаление моковых данных блога

--- src/modules/Blog/BlogComponents/ShortBlogSection/ShortBlogSection.tsx
+++ src/modules/Blog/BlogComponents/ShortBlogSection/ShortBlogSection.tsx
@@ -11,7 +11,7 @@ export const ShortBlogSection = memo(observer(() => {
   if (blogStore.error) {
     return null
   }
-  
+  if (!blogStore.isLoading && !blogStore.blogs?.publications.length) return null
   return (
     <Section className={classes.shortBlog} title='Последние публикации'>
       <div
@@ -19,20 +19,20 @@ export const ShortBlogSection = memo(observer(() => {
       >
         {blogStore.isLoading && <Loader />}
 
-        {blogStore.blogs?.publications.map((blog, index) => {
-          if (index >= 3) return null
-          return (
-            <MShortBlogCard
-              className={classes.blog}
-              key={blog.id}
-              blog={blog}
-              initial={ANIMATION_HIDDEN}
-              whileInView={ANIMATION_VISIBLE}
-              viewport={{ once: true, margin: "-140px" }}
-              variants={MotionUp}
-              custom={index}
-            />)
-        })}
+        { blogStore.blogs?.publications.map((blog, index) => {
+              if (index >= 3) return null
+              return (
+                <MShortBlogCard
+                  className={classes.blog}
+                  key={blog.id}
+                  blog={blog}
+                  initial={ANIMATION_HIDDEN}
+                  whileInView={ANIMATION_VISIBLE}
+                  viewport={{ once: true, margin: "-140px" }}
+                  variants={MotionUp}
+                  custom={index}
+                />)
+            })}
       </div>
     </Section>
 
